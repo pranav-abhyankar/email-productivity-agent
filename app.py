5ac33e0007904088024f2b942640138354a6b259
Pranav Pradeep Abhyankar <114946272+pranav-abhyankar@users.noreply.github.com>
2025-11-25 14:22:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,8 @@ with st.sidebar:
             st.error("Please configure Groq API key first")
 
 # Main content - Three columns
-col1, col2, col3 = st.columns()[1]
+col1, col2, col3 = st.columns([2, 2, 2])
+
 
 # Column 1: Email List
 with col1:
@@ -371,3 +372,4 @@ with col_stat3:
 with col_stat4:
     spam_count = len(st.session_state.db.get_emails_by_category('Spam'))
     st.metric("Spam", spam_count)
+
